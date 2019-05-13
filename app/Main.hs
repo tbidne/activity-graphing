700fdf8ad65e3a76f8960a615badd1a781333ebf
Thomas Bidne <tbidne@gmail.com>
2019-05-13 17:46:57 -0400

    switch hardcoded filepath to cmd line arg

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,29 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import qualified Parser as P
 import qualified Activities.Activity as A
-
+import System.Environment (getArgs)
 import Text.Megaparsec.Error as ME (errorBundlePretty)
 
 import Graphable
 
 main :: IO ()
-main = readFile "data/sample.txt" >>= graphOrDie . P.entry
+main = parseFileName >>= inHelper
+
+parseFileName :: IO (Maybe String)
+parseFileName = do
+  xs <- getArgs
+  case xs of
+    [s] -> return (Just s)
+    _ -> return Nothing
+
+inHelper :: Maybe String -> IO ()
+inHelper =
+  \case
+    Nothing -> putStrLn "Usage: stack exec graphing-exe <filename>"
+    Just s -> graphOrDie . P.entry =<< readFile s
 
 graphOrDie :: Either P.ParseErr [A.Activity] -> IO ()
 graphOrDie (Left l) = putStrLn $ ME.errorBundlePretty l
